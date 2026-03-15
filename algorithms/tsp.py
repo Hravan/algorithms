04ba50e6ddf3aac0d3c5ec355eb1069d83526372
Hravan <krzysztof.krenc@gmail.com>
2026-03-15 20:38:23 +0100

    Closest pair heuristic for the tsp.

--- algorithms/tsp.py
+++ algorithms/tsp.py
@@ -17,3 +17,46 @@ def nearest_neighbour(locations: list[int]) -> list[int]:
                 min_index = i
         location_order.append(locations.pop(min_index))
     return location_order
+
+
+def closest_pair(locations: list[int]):
+    '''Order the locations to visit using the closest pair heuristic.'''
+    n_points = len(locations)
+    chains = [[location] for location in locations]
+    while len(chains) != 1:
+        smallest_distance = float('inf')
+        for i_chain_a, chain_a in enumerate(chains):
+            for i_chain_b, chain_b in enumerate(chains[i_chain_a + 1:], start=i_chain_a+1):
+                start_a = chain_a[0]
+                end_a = chain_a[-1]
+                start_b = chain_b[0]
+                end_b = chain_b[-1]
+                for k, pair in enumerate([(end_a, start_b), (start_a, start_b), (start_a, end_b), (end_a, end_b)]):
+                    current_distance = abs(pair[0] - pair[1])
+                    print(current_distance)
+                    if current_distance < smallest_distance:
+                        i_to_connect_a = i_chain_a
+                        i_to_connect_b = i_chain_b
+                        i_pair = k
+                        smallest_distance = current_distance
+        connect_chains(chains, i_to_connect_a, i_to_connect_b, i_pair)
+    return chains[0]
+
+
+def connect_chains(chains, i_chain_a, i_chain_b, i_pair):
+    if i_pair == 0:
+        connect_chains(chains, i_chain_b, i_chain_a, 1)
+    elif i_pair == 1:
+        chain_b = chains[i_chain_b][::-1]
+        chain_b.extend(chains[i_chain_a])
+        chains.append(chain_b)
+        for i_chain in sorted([i_chain_a, i_chain_b], reverse=True):
+            del chains[i_chain]
+    elif i_pair == 2:
+        chains[i_chain_b].extend(chains[i_chain_a])
+        del chains[i_chain_a]
+    elif i_pair == 3:
+        chains[i_chain_a].extend(chains[i_chain_b][::-1])
+        del chains[i_chain_b]
+
+
--- tests/test_tsp.py
+++ tests/test_tsp.py
@@ -1,7 +1,14 @@
-from algorithms.tsp import nearest_neighbour
+from algorithms.tsp import nearest_neighbour, closest_pair
 
 
 def test_nearest_neighbour(mocker):
     locations = [-21, -5, -1, 0, 1, 3, 11]
     mocker.patch('random.randrange', return_value=1)
     assert nearest_neighbour(locations) == [-5, -1, 0, 1, 3, 11, -21]
+
+
+def test_closest_pair():
+    locations = [-10, -1, 0]
+    assert closest_pair(locations) == [-10, -1, 0]
+    locations = [-21, -5, -1, 0, 1, 3, 11]
+    assert closest_pair(locations) == [-21, -5, -1, 0, 1, 3, 11]
