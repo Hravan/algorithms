97972421c3bc5185568064c7ed0779b6c3b7ea4d
Hravan <krzysztof.krenc@gmail.com>
2026-05-07 17:51:17 +0200

    Single variable loop implementation.

--- algorithms/wiggle_sort.py
+++ algorithms/wiggle_sort.py
@@ -1,12 +1,15 @@
-from itertools import zip_longest
-
 def wiggle_sort(nums):
     nums.sort()
     n_odd = len(nums) // 2
     
     result = []
-    for even, odd in zip_longest(nums[:-n_odd], nums[-n_odd:]):
-        result.append(even)
-        if odd is not None:
-            result.append(odd)
+    i_even = 0
+    i_odd = 0
+    for i, _ in enumerate(nums):
+        if not i % 2:
+            result.append(nums[i_even])
+            i_even += 1
+        else:
+            result.append(nums[-n_odd:][i_odd])
+            i_odd += 1
     return result
